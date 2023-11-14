19aa124dc724b0ae32fbeda7b1ad9175d6370208
Francisco Casas <franciscocn17@gmail.com>
2023-11-13 18:47:34 -0600

    Primer programa curso java

---
+++ ProgramaJava/src/programajava/ProgramaJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programajava;
+
+/**
+ *
+ * @author Francisco Casas
+ */
+public class ProgramaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //INICIO PROGRAMA
+        System.out.println("HOLA HIJOS DE SU PENDEJA MADRE");
+        //FIN PROGRAMA
+    }
+    
+}
